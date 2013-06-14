948ef66f7f6d10c79581095f540ed9673aa0953e
Patrick Meiring <patrick.meiring@gmail.com>
2013-06-14 16:37:51 +1000

    Refactor location storage and handling.

---
+++ locationhandler.js
@@ -0,0 +1,37 @@
+﻿function create(callback) {
+    function handle(request, response) {
+        if (request.method !== "POST") {
+            response.send(405); // Method not Allowed
+            callback(new Error("Invalid HTTP Method used."));
+            return;
+        }
+        if (request.body.lat === undefined ||
+            request.body.lng === undefined ||
+            request.body.email === undefined) {
+
+            response.send(400);
+            callback(new Error("Invalid request body."));
+            return;
+        }
+        var email = request.body.email;
+        var latitude = parseFloat(request.body.lat);
+        var longitude = parseFloat(request.body.lng);
+
+        if (isNaN(latitude) || isNaN(longitude) || email.length < "a@a.eu".length) {
+
+            response.send(400);
+            callback(new Error("Invalid request data."));
+            return;
+        }
+        var userPosition = {
+            email: email,
+            latitude: latitude,
+            longitude: longitude,
+            lastUpdate: new Date()
+        };
+        response.send(200);
+        callback(undefined, userPosition);
+    }
+    return handle;
+}
+exports.create = create;
--- locationstore.js
+++ locationstore.js
@@ -1,54 +1,34 @@
-﻿var userPositions = [];
+﻿
 
-function location(request, response) {
-    if (request.method !== 'POST')
-        response.send(405); // Method not Allowed
-    if (request.body.lat === undefined ||
-        request.body.lng === undefined ||
-        request.body.email === undefined) {
-        response.send(400);
-        return;
+function create() {
+    var userPositions = [];
+
+    function setPosition(userPosition) {
+        var email = userPosition.email;
+        //userPositions[email] = userPosition; -- Avoiding this for reasons explained below.
+
+        for (var i = 0; i < userPositions.length; i++) {
+            if (userPositions[i].email === email) {
+                userPositions[i] = userPosition;
+                return;
+            }
+        }
+        userPositions.splice(userPositions.length, 0, userPosition);
     }
-    var email = request.body.email;
-    var latitude = parseFloat(request.body.lat);
-    var longitude = parseFloat(request.body.lng);
 
-    if (isNaN(latitude) || isNaN(longitude) || email.length < 'a@a.eu'.length) {
-        response.send(400);
-        return;
-    }
-    console.log("[LOCATION] Logged " + email + " at " + latitude + "," + longitude + " (lat, long).");
-    var userPosition = {
-            email: email,
-            latitude: latitude,
-            longitude: longitude,
-            lastUpdate: new Date()
-        };
-    setUserPosition(userPosition);
-    response.send(200);
-}
+    function getPosition(email) {
+        //return userPositions[email]; -- Avoiding this; if someone's email is "prototype" or "constructor" or something reserved, this could cause undesired operation.
 
-function setUserPosition(userPosition) {
-    var email = userPosition.email;
-    //userPositions[email] = userPosition; -- Avoiding this for reasons explained below.
-
-    for (var i = 0; i < userPositions.length; i++) {
-        if (userPositions[i].email === email) {
-            userPositions[i] = userPosition;
-            return;
+        for (var i = 0; i < userPositions.length; i++) {
+            if (userPositions[i].email === email) {
+                return userPositions[i];
+            }
         }
     }
-    userPositions.splice(userPositions.length, 0, userPosition);
-}
 
-function getUserPosition(email) {
-    //return userPositions[email]; -- Avoiding this; if someone's email is 'prototype' or 'constructor' or something reserved, this could cause undesired operation.
-
-    for (var i = 0; i < userPositions.length; i++) {
-        if (userPositions[i].email === email) {
-            return userPositions[i];
-        }
+    return {
+        setPosition: setPosition,
+        getPosition: getPosition
     }
 }
-
-exports.location = location;
+exports.create = create;
