cbfde2e0400efa56bd0541aa6c5975648dad3746
Patrick Meiring <patrick.meiring@gmail.com>
2013-06-28 11:45:09 +1000

    Use port 80 instead of 3000.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var config = require("./config.json");
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
