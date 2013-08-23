519c974ee6d66a324a49a80f8e2d90eb72a124c9
patrickmeiring <patrick.meiring@live.com>
2013-08-23 12:27:59 +1000

    Create utilities file.

---
+++ utils.js
@@ -0,0 +1,21 @@
+﻿exports.isString = function (value) {
+    return (value !== undefined && (typeof (value) === "string"));
+};
+exports.isNumber = function (value) {
+    return (value !== undefined && (typeof (value) === "number"));
+};
+exports.isDate = function (value) {
+    return (value !== undefined && (value instanceof Date));
+};
+exports.isObject = function (value) {
+    return (value !== undefined && (typeof (value) === "object"));
+};
+exports.isArray = function (value) {
+    return (value !== undefined && Array.isArray(value));
+};
+exports.isFunction = function (value) {
+    return (value !== undefined && (typeof (value) === "function"));
+};
+exports.isBoolean = function (value) {
+    return (value !== undefined && (typeof (value) === "boolean"));
+}
